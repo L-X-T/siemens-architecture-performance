4fa388148d37a57d3e78f12820ef6ea1cab493d1
Alex Thalhammer <alex@angular-software.at>
2021-11-12 23:06:33 +0100

    Updates & improvements

---
+++ apps/flight-admin/jest.config.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export default {
+  displayName: 'flight-admin',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  coverageDirectory: '../../coverage/apps/flight-admin',
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment'
+  ]
+};
---
+++ libs/flight-lib/jest.config.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export default {
+  displayName: 'flight-lib',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  coverageDirectory: '../../coverage/libs/flight-lib',
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment'
+  ]
+};
