fa3616f139fd13f34df633e07dda2ec736d2fab5
Alex Thalhammer <alex@angular-software.at>
2022-07-13 12:28:53 +0200

    [NxDDD] Bonus: Implement an UI Library

--- libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
+++ libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
+import { LuggageUiCardModule } from '@flight-workspace/luggage/ui-card';
 import { ReportLossComponent } from './report-loss.component';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule, RouterModule.forChild([{ path: '', component: ReportLossComponent }])],
+  imports: [CommonModule, LuggageDomainModule, LuggageUiCardModule, RouterModule.forChild([{ path: '', component: ReportLossComponent }])],
   declarations: [ReportLossComponent],
   exports: [ReportLossComponent]
 })
