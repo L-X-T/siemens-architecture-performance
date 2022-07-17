e86d5748fa5cff669f6fa3ed0a621491b9d2ada4
Alex Thalhammer <alex@angular-software.at>
2022-07-17 19:45:05 +0200

    [RxJS] Observable and Observer

--- apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-airport',
   templateUrl: './airport.component.html'
 })
-export class AirportComponent implements OnInit {
+export class AirportComponent {
   airports: string[] = [];
+  readonly airports$ = this.airportService.findAll();
+  private readonly airportsObserver: Observer<string[]> = {
+    next: (airports) => (this.airports = airports),
+    error: (err) => console.error(err),
+    complete: () => console.log('Observable completed!')
+  };
 
-  constructor(private airportService: AirportService) {}
+  constructor(private airportService: AirportService) {
+    this.subscribeToAirports();
+  }
 
-  ngOnInit(): void {
-    this.airportService.findAll().subscribe((airports) => {
-      this.airports = airports;
-    });
+  private subscribeToAirports(): void {
+    this.airports$.subscribe(this.airportsObserver);
   }
 }
