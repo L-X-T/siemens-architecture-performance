44e9c11cc8d6fd1544ad7ceeb46cd9950065c0bb
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-01-31 14:57:27 +0100

    [NgRx] Linting files

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './+state';
+import { metaReducers, reducers } from './+state';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
