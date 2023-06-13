L-X-T/buhlergroup

[NgRx] Selectors Bonus: Using feature selectors

apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -1,13 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromFlightBooking from './flight-booking.reducer';
-import { FlightBookingAppState } from './flight-booking.reducer';
 
-/*export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
-  fromFlightBooking.flightBookingFeatureKey
-);*/
+export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(fromFlightBooking.flightBookingFeatureKey);
 
-export const selectFlights = (s: FlightBookingAppState) => s.flightBooking.flights;
-export const negativeList = (s: FlightBookingAppState) => s.flightBooking.negativeList;
+export const selectFlights = createSelector(selectFlightBookingState, (s) => s.flights);
+export const negativeList = createSelector(selectFlightBookingState, (s) => s.negativeList);
 
 export const selectedFilteredFlights = createSelector(selectFlights, negativeList, (flights, negativeList) =>
   flights.filter((f) => !negativeList.includes(f.id))

L-X-T/siemens-architecture-performance

apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -1,13 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromFlightBooking from './flight-booking.reducer';
-import { FlightBookingAppState, flightBookingFeatureKey } from './flight-booking.reducer';
 
-/*export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
-  fromFlightBooking.flightBookingFeatureKey
-);*/
+export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(fromFlightBooking.flightBookingFeatureKey);
 
-export const selectFlights = (appState: FlightBookingAppState) => appState[flightBookingFeatureKey].flights;
-export const negativeList = (appState: FlightBookingAppState) => appState[flightBookingFeatureKey].negativeList;
+export const selectFlights = createSelector(selectFlightBookingState, (featureState) => featureState.flights);
+export const negativeList = createSelector(selectFlightBookingState, (featureState) => featureState.negativeList);
 
 export const selectFilteredFlights = createSelector(selectFlights, negativeList, (flights, negativeList) =>
   flights.filter((f) => !negativeList.includes(f.id))