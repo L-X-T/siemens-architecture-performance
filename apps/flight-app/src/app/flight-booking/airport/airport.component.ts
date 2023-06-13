65417a48e5c39db39ec7e00611d5b426b8a46f73
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-06-13 10:34:33 +0200

    [RxJS] 1. subscription object

--- apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
-import { Observer } from 'rxjs';
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-airport',
   templateUrl: './airport.component.html'
 })
-export class AirportComponent {
+export class AirportComponent implements OnDestroy {
   airports: string[] = [];
   readonly airports$ = this.airportService.findAll();
   private readonly airportsObserver: Observer<string[]> = {
@@ -15,11 +16,21 @@ export class AirportComponent {
     complete: () => console.log('Observable completed!')
   };
 
+  // 1 subscription object
+  private readonly subscription = new Subscription();
+
   constructor(private airportService: AirportService) {
     this.subscribeToAirports();
   }
 
+  ngOnDestroy(): void {
+    // 1 subscription object
+    this.subscription.unsubscribe();
+  }
+
   private subscribeToAirports(): void {
-    this.airports$.subscribe(this.airportsObserver);
+    // 1 subscription object
+    this.subscription.add(this.airports$.subscribe(this.airportsObserver));
+    // this.subscription.add(this.airports$.subscribe(this.airportsObserver)); // 2nd
   }
 }
