L-X-T/god-reactive

Updates & improvements

jest.config.ts
@@ -1,0 +1,5 @@
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = {
+  projects: getJestProjects()
+};

L-X-T/siemens-architecture-performance

jest.config.ts
@@ -1,0 +1,5 @@
+const { getJestProjects } = require('@nx/jest');
+
+module.exports = {
+  projects: getJestProjects()
+};